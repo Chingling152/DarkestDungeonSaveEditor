5697cc7ae4273f0e38773786a298f577eb495939
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-05-12 12:42:57 -0300

    Sets Hero's icon as Panel

--- src/main/java/de/robojumper/ddsavereader/v2/ui/components/hero/icon/HeroIconUIComponent.java
+++ src/main/java/de/robojumper/ddsavereader/v2/ui/components/hero/icon/HeroIconUIComponent.java
@@ -1,13 +1,15 @@
 package de.robojumper.ddsavereader.v2.ui.components.hero.icon;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class HeroIconUIComponent extends JComponent {
+public class HeroIconUIComponent extends JPanel {
     private final String heroClass;
     private final Integer colorVariant;
 
@@ -19,20 +21,12 @@ public class HeroIconUIComponent extends JComponent {
         this.initialize();
     }
 
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(100, 100);
-    }
-
     private void initialize(){
-        this.setSize(100, 100);
         try {
             BufferedImage image = ImageIO.read(Objects.requireNonNull(getClass().getResource("plague_doctor/plague_doctor_portrait_roster_0.png")));
-            JLabel picLabel = new JLabel();
-            picLabel.setIcon(new ImageIcon(image));
-            picLabel.setText("jLabel");
+            JLabel picLabel = new JLabel(new ImageIcon(image));
             picLabel.setVisible(true);
-            add(picLabel);
+            this.add(picLabel);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
